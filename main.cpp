8e19c4f30bcc6e3306328d5bba50806cf7558083
Taha <t.abedinidevhub@gmail.com>
2025-11-09 23:08:51 +0330

    c++ 22

--- main.cpp
+++ main.cpp
@@ -4,14 +4,20 @@ using namespace std;
 
 int main()
 {
-    int num,tedad=0,mx=-1;
-while(tedad<5){
-    cout<<"enter number:"<<endl;
-    cin>>num;
-    if(num>mx)
-        mx=num;
-    tedad++;
-}
-cout<<"maximum is:"<<mx;
+    int m;
+int num[3]={12,8,9};
+for(int i=0;i<3;i++)
+    cout<<num[i]<<endl;
+
+    int sizen;
+
+    cout<<"enter number:";
+    cin>>sizen;
+int num[sizen];
+    for(int i=0;i<sizen;i++)
+    cin>>num[i];
+
+    for(int i=0;i<sizen;i++)
+        cout<<num[i]<<endl;
     return 0;
 }
