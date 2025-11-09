61fffb17d20e85ffacd954c0a9d79842600d3c16
Taha <t.abedinidevhub@gmail.com>
2025-11-09 22:34:56 +0330

    c++ 7

--- main.cpp
+++ main.cpp
@@ -4,25 +4,11 @@ using namespace std;
 
 int main()
 {
-   float a,b;
-            cout<<"welcome to BMI  "<<endl;
-            cout<<"enter your height(meter): ";
-            cin>>a;
-            cout<<"enter your weight(kg): ";
-            cin>>b;
-            cout<<b/(a*a)<<endl;
-            float bmi=b/(a*a);
-            if(bmi>=40){
-                cout<<"fat degree 3";}
-            else if(bmi<=39.9 && bmi>=35) {
-                cout<<"fat degree 2";}
-            else if(bmi<=34.9 && bmi>=30){
-                cout<<"fat degree 1";}
-            else if(bmi<=29.9 && bmi>=25){
-                cout<<"overweight";}
-            else if(bmi<=24.9 && bmi>=18.5){
-                cout<<"normal";}
-            else if(bmi<18.5){
-                cout<<"underweight";}
+    int a;
+             cout<<"enter number:";
+             cin>>a;
+
+             for(int b=1;b<=a;b++)
+             if(a % b==0) cout<<"maghsom elayh"<<a<<"="<<b<<endl;
     return 0;
 }
