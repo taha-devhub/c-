2c119d55993e60c4f5604ff389e03db96ba61d04
Taha <t.abedinidevhub@gmail.com>
2025-11-09 22:35:23 +0330

    c++ 8

--- main.cpp
+++ main.cpp
@@ -4,11 +4,20 @@ using namespace std;
 
 int main()
 {
-    int a;
-             cout<<"enter number:";
-             cin>>a;
+    int num;
+             int sum=0;
+             int s=0;
 
-             for(int b=1;b<=a;b++)
-             if(a % b==0) cout<<"maghsom elayh"<<a<<"="<<b<<endl;
+             cout<<"enter number:";
+             cin>>num;
+
+             cout<<"magsom elayh"<<num<<"="<<endl;
+             for(int x=1;x<=num;x++)
+                if(num % x==0){
+                sum=sum+1;
+                s=s+x;
+             cout<<x<<endl;}
+             cout<<"tedad="<<sum<<endl;
+             cout<<"sum magsom elayh="<<s;
     return 0;
 }
