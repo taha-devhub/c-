758ecb8eb8520bf9c0f278766d83e24fc2410591
Taha <t.abedinidevhub@gmail.com>
2025-11-09 23:12:07 +0330

    c++ 24

--- main.cpp
+++ main.cpp
@@ -4,21 +4,26 @@ using namespace std;
 
 int main()
 {
-   int n;
-            cout<<"tadad:";
-            cin>>n;
-            string lname[n];
-            string fname[n];
+    int n;
+                cout<<"tedad: ";
+                cin>>n;
 
-            for(int i=0;i<n;i++)
-             {cout<<"enter first name: "<<endl;
-            cin>>fname[i];}
+                string name[n];
+                for(int i=0;i<n;i++)
+                {
+                    cout<<"enter name: \n";
+                    cin>>name[i];
+                }
+                string findn;
+                cout<<"enter find your name:";
+                cin>>findn;
 
-            for(int i=0;i<n;i++)
-             {cout<<"enter last name: "<<endl;
-            cin>>lname[i];}
-
-             for(int i=0;i<n;i++)
-                {cout<<fname[i]<<" "<<lname[i]<<endl;}
+                for(int i=0;i<n;i++)
+                {
+                    string x=name[i];
+                    if(findn == x){
+                        cout<<" find "<<findn<<"index"<<i;
+                    }else
+                    cout<<"not find";}
     return 0;
 }
