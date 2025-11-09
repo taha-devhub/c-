58dd439e6f72e3c03f18a18b26156b1a1c1476f8
Taha <t.abedinidevhub@gmail.com>
2025-11-09 22:41:11 +0330

    c++ 10

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-   int n,tedad=0;
+   int n,tedad=0,sum=0;
 
              cin>>n;
 
@@ -12,9 +12,23 @@ int main()
                              if(n%a==0)
              {
                  tedad++;
+                 sum+=a;
                  cout<<tedad<<")"<<a<<endl;
 
              }
-             cout<<"tedad adad="<<tedad;
+             cout<<"tedad adad="<<tedad<<endl;
+             cout<<"magmooe="<<sum;
+
+
+             int n;
+             cout<<"enter number 1:"<<endl;
+             cin>>n;
+             int m;
+             cout<<"enter number 2:"<<endl;
+             cin>>m;
+             int miny=min(n,m);
+             int maxy=max(n,m);
+             for(int c=miny;c<maxy;c++)
+                cout<<c<<endl;
     return 0;
 }
