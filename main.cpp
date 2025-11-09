540d5fa42456b49d8f277a0edda267aa207448a6
Taha <t.abedinidevhub@gmail.com>
2025-11-09 22:45:54 +0330

    c++ 12

--- main.cpp
+++ main.cpp
@@ -4,14 +4,17 @@ using namespace std;
 
 int main()
 {
-int a=0,b=0;
-             cout<<"enter number 1:";
-             cin>>a;
-             for(int x=1;x<=a;x++)
-                if(a%x==0){
-                        b=b+1;
+    int a;
+                int b=0;
+                cout<<"enter number:";
+                cin>>a;
 
-                cout<<"maghsom alayh"<<a<<"="<<x<<endl;
-                cout<<"tedad="<<b;}
+                for(int x=1;x<=a;x++)
+                    if(a%x==0){
+                    b=b+1;}
+                    if(b==2)
+                        cout<<"number one";
+                    else
+                        cout<<"morakab";
     return 0;
 }
