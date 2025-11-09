daf7f5e9ee49e6bce7e0a8f8c647a36b23f7ae42
Taha <t.abedinidevhub@gmail.com>
2025-11-09 22:57:57 +0330

    c++ 17

--- main.cpp
+++ main.cpp
@@ -4,11 +4,16 @@ using namespace std;
 
 int main()
 {
-    for(int a=1;a<=10;a++){
-                for(int b=1;b<=10;b++)
-                    cout<<a*b<<"   ";
-                cout<<endl;
+    int n1,n2;
+            cout<<"enter first number:"<<endl;
+            cin>>n1;
+            cout<<"enter second number:"<<endl;
+            cin>>n2;
 
+            for(int i=1;i<=n1;i++)
+            {
+                if(n1%i==0 && n2%i==0)
+                    cout<<i<<endl;
             }
     return 0;
 }
