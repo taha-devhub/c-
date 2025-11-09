20c38356ddc6c5371826559e5a3eeffb50411e95
Taha <t.abedinidevhub@gmail.com>
2025-11-09 23:13:38 +0330

    c++ 25

--- main.cpp
+++ main.cpp
@@ -4,26 +4,33 @@ using namespace std;
 
 int main()
 {
-    int n;
-                cout<<"tedad: ";
-                cin>>n;
+    int n,findn,index,countn=0;
+                        int andis[100];
+                        cout<<"enter number:"<<endl;
+                        cin>>n;
+                        int numbers[n];
+                        for(int i=0;i<n;i++)
+                        {
+                            //read array
+                            cout<<"enter number:"<<i+1<<":";
+                            cin>>numbers[i];
 
-                string name[n];
-                for(int i=0;i<n;i++)
-                {
-                    cout<<"enter name: \n";
-                    cin>>name[i];
-                }
-                string findn;
-                cout<<"enter find your name:";
-                cin>>findn;
+                        }
+                        //print array
+                        for(int i=0;i<n;i++)
+                            cout<<setw(4)<<numbers[i];
+                        cout<<"\n enter find number";
+                        cin>>findn;
+                        for(int i=0;i<n;i++)
+                            if(findn==numbers[i])
+                            {andis[countn]=i;
+                            countn++;
+                            }
+                            if(countn==0)
+                                cout<<"not found"<<index;
+                            else
+                                for(int i=0;i<countn;i++)
 
-                for(int i=0;i<n;i++)
-                {
-                    string x=name[i];
-                    if(findn == x){
-                        cout<<" find "<<findn<<"index"<<i;
-                    }else
-                    cout<<"not find";}
+                            cout<<andis[i]<<"\t";*/
     return 0;
 }
