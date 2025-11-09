5e8f64d609e08f26f8c1afbbd136509760ce9b7a
Taha <t.abedinidevhub@gmail.com>
2025-11-09 23:16:12 +0330

    c++ 26

--- main.cpp
+++ main.cpp
@@ -4,33 +4,14 @@ using namespace std;
 
 int main()
 {
-    int n,findn,index,countn=0;
-                        int andis[100];
-                        cout<<"enter number:"<<endl;
-                        cin>>n;
-                        int numbers[n];
-                        for(int i=0;i<n;i++)
-                        {
-                            //read array
-                            cout<<"enter number:"<<i+1<<":";
-                            cin>>numbers[i];
+    string n;
+                            int len;
+                            cout<<"ener name:"<<endl;
+                            getline(cin,n);
 
-                        }
-                        //print array
-                        for(int i=0;i<n;i++)
-                            cout<<setw(4)<<numbers[i];
-                        cout<<"\n enter find number";
-                        cin>>findn;
-                        for(int i=0;i<n;i++)
-                            if(findn==numbers[i])
-                            {andis[countn]=i;
-                            countn++;
-                            }
-                            if(countn==0)
-                                cout<<"not found"<<index;
-                            else
-                                for(int i=0;i<countn;i++)
-
-                            cout<<andis[i]<<"\t";*/
+                            cout<<n.length()<<endl;
+                            len=n.length();
+                            for(int i=0; i<len;i++)
+                                cout<<n[i]<<"\t";
     return 0;
 }
