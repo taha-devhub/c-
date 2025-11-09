b48e348daff19fae6fe009945bcde40d979c40d9
Taha <t.abedinidevhub@gmail.com>
2025-11-09 22:56:14 +0330

    c++ 16

--- main.cpp
+++ main.cpp
@@ -4,22 +4,11 @@ using namespace std;
 
 int main()
 {
-    int tedad=0;
+    for(int a=1;a<=10;a++){
+                for(int b=1;b<=10;b++)
+                    cout<<a*b<<"   ";
+                cout<<endl;
 
-           for(int a=1;a<=1000;a+=2)
-           {
-               tedad++;
-               cout<<tedad<<") "<<a<<endl;
-           }
-
-
-
-
-           for(int a=1;a<=19;a++)
-            {
-                for(int b=1;b<=a;b++)
-            cout<<"*";
-           cout<<endl;
             }
     return 0;
 }
