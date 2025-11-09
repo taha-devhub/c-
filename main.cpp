ed9d60a5b796f9c7a452d21de251db7882b79b07
Taha <t.abedinidevhub@gmail.com>
2025-11-09 21:55:02 +0330

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{int number;
+     cout<<"enter your number";
+     cin>>number;
+      if(number>0)
+        cout<<"posetive";
+      else if(number< 0)
+        cout<<"negetive";
+      else if(number==0)
+        cout<<"zero";
+
+    return 0;
+}
