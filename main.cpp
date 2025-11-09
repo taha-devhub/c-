0913001ebf514af2b7c3b4a922fb3fa0a2260162
Taha <t.abedinidevhub@gmail.com>
2025-11-09 22:59:57 +0330

    c++ 18

--- main.cpp
+++ main.cpp
@@ -4,16 +4,16 @@ using namespace std;
 
 int main()
 {
-    int n1,n2;
-            cout<<"enter first number:"<<endl;
-            cin>>n1;
-            cout<<"enter second number:"<<endl;
-            cin>>n2;
-
-            for(int i=1;i<=n1;i++)
-            {
-                if(n1%i==0 && n2%i==0)
-                    cout<<i<<endl;
-            }
+    int a;
+             while(true)
+             {
+                 cout<<"enter number:";
+                 cin>>a;
+                 if(a%2==0)
+                    cout<<"even\n";
+                 else
+                    cout<<"odd\n";
+                 if(a==-1)
+                    break;}
     return 0;
 }
